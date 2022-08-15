0a4003701692e102df57fff1620a285ee407bf54
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-08-15 20:40:19 +0300

    Versioning

---
+++ src/interviewTasks/TransformStringHvingNoDuplications/StringWithNoDuplications.java
@@ -0,0 +1,73 @@
+package interviewTasks.TransformStringHvingNoDuplications;
+
+//(removeDupes('abcd')) // -> 'abcd'
+//(removeDupes('aabbccdd')) // -> 'abcd'
+//(removeDupes('abcddbca')) // -> 'abcd'
+//(removeDupes('abababcdcdcd')) // -> 'abcd'
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringWithNoDuplications {
+
+    static String s1 = "abcd";
+    static String s2 = "aabbccdd";
+    static String s3 = "abcddbca";
+    static String s4 = "abcddbca";
+
+    public static void main(String[] args) {
+
+        System.out.println(removeDupes(s1));
+        System.out.println(removeDupes(s2));
+        System.out.println(removeDupes(s3));
+        System.out.println(removeDupes(s4));
+        System.out.println();
+        System.out.println(removeDupesV2(s1));
+        System.out.println(removeDupesV2(s2));
+        System.out.println(removeDupesV2(s3));
+        System.out.println(removeDupesV2(s4));
+    }
+
+    // var1:
+    static String removeDupes(String stringToRemoveDuplications) {
+
+        String stringWithoutDuplications = new String();
+
+        char[] charsArrayFromString = stringToRemoveDuplications.toCharArray();
+        List<Character> characterList = new ArrayList<>();
+
+        for (char c : charsArrayFromString) {
+            characterList.add(c);
+        }
+
+        for (int i = 0; i < characterList.size(); i++) {
+            for (int j = i + 1; j < characterList.size(); j++) {
+                if (characterList.get(i) == characterList.get(j)){
+                    characterList.remove(j);
+                }
+            }
+        }
+
+        for (char c : characterList) {
+            stringWithoutDuplications += c;
+        }
+
+        return stringWithoutDuplications;
+    }
+
+    // var2:
+
+    static String removeDupesV2(String stringToRemoveDuplications) {
+
+    return stringToRemoveDuplications.chars()
+            .mapToObj(c -> (char) c)
+            .collect(Collectors.toSet())
+            .stream()
+            .map(e->e.toString())
+            .collect(Collectors.joining());
+    }
+}
--- src/interviewTasks/flattenArrayFromFewArrays/FlattenArray.java
+++ src/interviewTasks/flattenArrayFromFewArrays/FlattenArray.java
@@ -1,7 +1,10 @@
 package interviewTasks.flattenArrayFromFewArrays;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class FlattenArray {
 
@@ -15,9 +18,14 @@ public class FlattenArray {
     public static void main(String[] args) {
 
         System.out.println(Arrays.toString(flattenarray(testArrayOfArrays)));
+        System.out.println();
+        System.out.println(Arrays.toString(flattenarrayV2(testArrayOfArrays)));
+        System.out.println();
+//        System.out.println(Arrays.toString(flattenarrayV3(testArrayOfArrays)));
 
     }
 
+    // var1:
     static int[] flattenarray(int[][] arrayOfArrays) {
 
         int resArrayLength = 0;
@@ -37,4 +45,53 @@ public class FlattenArray {
         }
         return resAray;
     }
+
+    // var2 (using colllections):
+
+    static int[] flattenarrayV2(int[][] arrayOfArrays) {
+
+        List list = new ArrayList();
+
+        for (int i = 0; i < arrayOfArrays.length; i++) {
+            for (int elemennt : arrayOfArrays[i]) {
+                list.addAll(Arrays.asList(elemennt));
+            }
+        }
+
+//        Object[] resList = list.toArray().a;
+
+        return list.stream().mapToInt(j -> (int) j).toArray();
+    }
+
 }
+
+
+//// var3 (using streams):
+//example:
+//public class MergeArrayExample5
+//{
+//    // function to merge two arrays
+//    public static <T> Object[] mergeArray(T[] arr1, T[] arr2)
+//    {
+//        return Stream.of(arr1, arr2).flatMap(Stream::of).toArray();
+//    }
+//    public static void main (String[] args)
+//    {
+//        Integer[] firstArray = new Integer[]{13,12,11,6,9,3}; //source array
+//        Integer[] secondArray = new Integer[]{78,34,56,67,2,11,7}; //destination array
+//        Object[] mergedArray = mergeArray(firstArray,secondArray); //merged array
+//        System.out.println("Merged array: "+ Arrays.toString(mergedArray));
+//    }
+//}
+//
+//    static int[] flattenarrayV3(int[][] arrayOfArrays) {
+//
+////        int resArrayLength = 0;
+////
+////        for (int i = 0; i < arrayOfArrays.length; i++) {
+////            resArrayLength += arrayOfArrays[i].length;
+////        }
+//
+//        return Arrays.stream(Stream.of(Stream.of(arrayOfArrays)).flatMap(Stream::of).toArray()).mapToInt(j -> (int) j).toArray();
+//    }
+//}
