38acbea9555e2555e61679797b01183309536472
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-08-16 01:46:45 +0300

    Versioning

---
+++ src/interviewTasks/mostFequentStringInArray/MostFrequentString.java
@@ -0,0 +1,64 @@
+package interviewTasks.mostFequentStringInArray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MostFrequentString {
+
+//    Какая строка встречается чаще всего
+//    Напишите функцию, которая принимает массив строк и возвращает самую частовстречающуюся
+//    строку в этом массиве
+//    Если таких строк несколько, то нужно вернуть первую, идя слева на право.
+//    **Input**: String[]
+//    **Output**: String
+//    function highestFrequency(array) {
+//        // todo
+//    }
+//
+//            console.log(highestFrequency([""a"", ""b"", ""c"", ""c"", ""d"", ""e""])) // -> c
+//            console.log(highestFrequency([""abc"", ""def"", ""abc"", ""def"", ""abc""])) // -> abc
+//            console.log(highestFrequency([""abc"", ""def""])) // -> abc
+//            console.log(highestFrequency([""abc"", ""abc"", ""def", ""def"", ""def"", ""ghi"", ""ghi"", ""ghi"", ""ghi"" ])) // -> ghi
+
+    static String[] str1 = {"a", "b", "c", "c", "d", "e"};
+    static String[] str2 = {"abc", "def", "abc", "def", "abc"};
+    static String[] str3 = {"abc", "def"};
+    static String[] str4 = {"abc", "abc", "def", "def", "def", "ghi", "ghi", "ghi", "ghi"};
+
+    public static void main(String[] args) {
+
+        System.out.println(mostFrequent(str1));
+        System.out.println(mostFrequent(str2));
+        System.out.println(mostFrequent(str3));
+        System.out.println(mostFrequent(str4));
+    }
+
+    static String mostFrequent(String[] inputString) {
+
+        String mostFrequentString = new String();
+        int maxQuantity = 0;
+
+        Map<String, Integer> workingMap = new HashMap<String, Integer>();
+
+        for (int i = 0; i < inputString.length; i++) {
+            if (workingMap.containsKey(inputString[i])) {
+                workingMap.put(inputString[i], workingMap.get(inputString[i]) + 1);
+//                mostFrequentString = inputString[i];
+//                maxQuantity = workingMap.get(inputString[i]) + 1;
+            } else {
+                workingMap.put(inputString[i], 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : workingMap.entrySet()) {
+            if (entry.getValue() > maxQuantity){
+                maxQuantity = entry.getValue();
+                mostFrequentString = entry.getKey();
+            }
+
+        }
+
+        return mostFrequentString;
+    }
+
+}
