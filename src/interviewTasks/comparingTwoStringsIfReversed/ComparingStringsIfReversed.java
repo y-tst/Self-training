b57bf9235755c75941426a81aebc2f90c995d785
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-08-21 03:04:00 +0300

    add rotated check

--- src/interviewTasks/comparingTwoStringsIfReversed/ComparingStringsIfReversed.java
+++ src/interviewTasks/comparingTwoStringsIfReversed/ComparingStringsIfReversed.java
@@ -1,27 +1,65 @@
 package interviewTasks.comparingTwoStringsIfReversed;
 
+import java.util.ArrayList;
+
 public class ComparingStringsIfReversed {
 
+    // for isRotated():
 //'javascript', 'scriptjava')) // -> true
 //'javascript', 'iptjavascr')) // -> true
 //'javascript', 'java')) // -> false
 
     public static void main(String[] args) {
 
-        System.out.println(isReversed("javascript", "tpircsavaj"));
-        System.out.println(isReversed("javascript", "iptjavascr"));
-        System.out.println(isReversed("javascript", "java"));
-        System.out.println(isReversed("abc123", "321cba"));
+//        System.out.println(isReversed("javascript", "tpircsavaj"));
+//        System.out.println(isReversed("javascript", "iptjavascr"));
+//        System.out.println(isReversed("javascript", "java"));
+//        System.out.println(isReversed("abc123", "321cba"));
+
+        System.out.println();
+
+        System.out.println(isRrotated("javascript", "scriptjava"));
+        System.out.println(isRrotated("javascript", "iptjavascr"));
+        System.out.println(isRrotated("javascript", "java"));
+        System.out.println(isRrotated("test", "java"));
 
     }
 
-    public static boolean isReversed(String firstStrind, String secondString) {
+    public static boolean isReversed(String firstString, String secondString) {
 
         StringBuilder reversedSecondString = new StringBuilder();
 
         for (int i = secondString.length() - 1; i >= 0; i--) {
             reversedSecondString = reversedSecondString.append(secondString.toCharArray()[i]);
         }
-        return firstStrind.equals(reversedSecondString.toString());
+        return firstString.equals(reversedSecondString.toString());
+    }
+
+    public static boolean isRrotated(String firstString, String secondString) {
+
+        if (firstString.length() != secondString.length()) {
+            return false;
+        }
+
+
+        ArrayList<Character> firstStringToArrayList = new ArrayList<>();
+        ArrayList<Character> secondStringToArrayList = new ArrayList<>();
+
+        for (char c : firstString.toCharArray()) {
+            firstStringToArrayList.add(c);
+        }
+
+        for (char c : secondString.toCharArray()) {
+            secondStringToArrayList.add(c);
+        }
+
+        for (int i = 0; i < firstStringToArrayList.size(); i++) {
+            firstStringToArrayList.add(firstStringToArrayList.get(0));
+            firstStringToArrayList.remove(0);
+            if (firstStringToArrayList.equals(secondStringToArrayList)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
