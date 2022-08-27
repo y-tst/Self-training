36a2b6beb15f1232c3dea271fffa8ccb81a20b0f
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-08-27 23:21:20 +0300

    add comparing arrays

---
+++ src/interviewTasks/arraysItemsSimilarity/ComparingArraysItems.java
@@ -0,0 +1,51 @@
+package interviewTasks.arraysItemsSimilarity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ComparingArraysItems {
+
+//    console.log(arraySubset([2, 1, 3], [1, 2, 3])) // -> true
+//    console.log(arraySubset([2, 1, 1, 3], [1, 2, 3])) // -> true
+//    console.log(arraySubset([1, 1, 1, 3], [1, 3, 3])) // -> false
+//    console.log(arraySubset([1, 2], [1, 2, 3])) // -> false
+
+    public static void main(String[] args) {
+        System.out.println(isFirstArrayIncludesItemsFromSecondArray(new int[]{2, 1, 3}, new int[]{1, 2, 3}));
+        System.out.println(isFirstArrayIncludesItemsFromSecondArray(new int[]{2, 1, 1, 3}, new int[]{1, 2, 3}));
+        System.out.println(isFirstArrayIncludesItemsFromSecondArray(new int[]{1, 1, 1, 3}, new int[]{1, 3, 3}));
+        System.out.println(isFirstArrayIncludesItemsFromSecondArray(new int[]{1, 2}, new int[]{1, 2, 3}));
+        System.out.println(isFirstArrayIncludesItemsFromSecondArray(new int[]{2, 1, 1, 3}, new int[]{5}));
+  }
+
+    public  static  boolean isFirstArrayIncludesItemsFromSecondArray(int[] arr1, int[] arr2){
+
+        if (arr1.length < arr2.length)
+            return  false;
+
+//  simple variant How to convert int[] into List<Integer> in Java:
+//
+//        int[] ints = {1, 2, 3};
+//        List<Integer> intList = new ArrayList<Integer>(ints.length);
+//        for (int i : ints)
+//        {
+//            intList.add(i);
+//        }
+
+        // via streams:
+
+        List<Integer> arrayList = Arrays.stream(arr1).boxed().collect(Collectors.toList());
+
+        for (int i = 0; i < arr2.length; i++)
+        {
+            if (arrayList.contains(arr2[i])){
+               arrayList.remove(arrayList.indexOf(arr2[i]));
+            }
+            else
+                return  false;
+        }
+        return  true;
+    }
+}
