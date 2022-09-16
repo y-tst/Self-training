673fb3f58109732ec9da631441bf0d029c04a151
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-09-17 00:52:28 +0300

    queue realisation 1st variant

--- src/interview_tasks/queue_realisation/QueueViaHashTable.java
+++ src/interview_tasks/queue_realisation/QueueViaHashTable.java
@@ -2,8 +2,13 @@ package interview_tasks.queue_realisation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class QueueViaHashTable {
+
+    private static List<Integer> list = new ArrayList<>();
+    private static int first = 0;
+    private static int last = 0;
 
     public static void main(String[] args) {
 
@@ -11,31 +16,38 @@ public class QueueViaHashTable {
         TestQueue.enqueue(20);
         TestQueue.enqueue(30);
 
-        System.out.println((TestQueue.list);
+        System.out.println(list.stream().map(Object::toString).collect(Collectors.joining(", ")));
+
+        TestQueue.dequeue();
+
+        System.out.println(list.stream().map(Object::toString).collect(Collectors.joining(", ")));
+
+        System.out.println("Queue size: " + TestQueue.size());
 
 
     }
 
-    private static List<Integer> list = new ArrayList<>();
-    private static int first = 0;
-    private static int last = 0;
+    public static class TestQueue {
 
-    public static class  TestQueue {
+        static void enqueue(int item) {
+            list.add(last, item);
+            last++;
+        }
 
-       static void  enqueue(int item){
-           list.set(last, item);
-           last++;
-       }
+        static void dequeue() {
+            if (list.isEmpty()) {
+                System.out.println("Queue is empty");
+                return;
+            }
+            int firstQueueItem = list.get(first);
+            list.remove(first);
+            last--;
 
-       static void dequeue(){
-           if (list.isEmpty())  {
-               return;
-           }
-           list.remove(first);
-       }
+            System.out.println("Removed element: " + firstQueueItem);
+        }
 
-       static int size(){
-           return last;
-       }
+        static int size() {
+            return last;
+        }
     }
 }
