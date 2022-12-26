2213439219bdccd7384c7e42f12f8a0734d4e0c4
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-12-26 19:57:26 +0200

    Regex training task2

--- src/regular_expressions/SearchXNumbers.java
+++ src/regular_expressions/SearchXNumbers.java
@@ -6,5 +6,23 @@ package regular_expressions;
 с помощью регулярных выражений и выводящую их на страницу.
  */
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SearchXNumbers {
+
+    public static void main(String[] args) {
+
+        int value = 0X25f5;
+
+        String regex = "0[Xx][0-9a-fA-F]+";
+        String input = "dfsgadf 0X23f1 oihjoijhoi 0x12 hohoih 0XAB";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
 }
