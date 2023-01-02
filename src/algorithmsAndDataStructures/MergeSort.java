6bfd7aa85c4d196e8cdb783b30e3a1b4ab026daa
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-01-02 20:26:04 +0200

    add merge sort

---
+++ src/algorithmsAndDataStructures/MergeSort.java
@@ -0,0 +1,14 @@
+package algorithmsAndDataStructures;
+
+import java.util.Random;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+
+    }
+}
